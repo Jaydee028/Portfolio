a2c690f3607568bde210ee14ff0cb159c3fd4df1
Jesse Dan P. Bancale <jessedanbancale@gmail.com>
2025-03-05 12:15:27 +0800

    remake

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <footer class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-        <div class="container p-8 flex justify-center items-center">
-            <!-- <div class="text-white text-3xl font-bold">LOGO</div> -->
-            <img class="h-12 w-[13em] px-auto" src="/image/Logo2.png" alt="menu">
-            <!-- <p class="text-slate-600">All rights reserved.</p> -->
-        </div>
-    </footer>
-</template>
