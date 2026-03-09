3412c0a275631903140889e4a3d8128c984ab528
JDCodeJungler <136150816+Jaydee028@users.noreply.github.com>
2026-03-09 21:38:52 +0800

    day 1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,9 @@
 <template>
   <footer class="mt-8 border-t border-[#33353F] bg-gray-900 text-white py-6">
-    <div class="flex w-full justify-center items-center">
-      <img class="h-14 w-[13em] transition-transform duration-300 hover:scale-105" src="/image/Logo2.png"
-        alt="CodeJungler" />
-    </div>
-    <p class="text-center text-gray-500 text-sm mt-2">
+    <!-- <div class="flex w-full justify-center items-center">
+      <img class="h-14 w-[13em] transition-transform duration-300 hover:scale-105" src="/image/Logo2.png"alt="CodeJungler" />
+    </div> -->
+    <p class="text-center text-gray-500 text-sm mt-0">
       © {{ currentYear }} Jesse Dan P. Bancale, MIT
     </p>
     <!-- <p class="text-center text-gray-500 text-xs mt-1">
@@ -22,14 +21,14 @@ const visitorCount = ref(0);
 
 // Load from .env
 const BIN_ID = import.meta.env.VITE_BIN_ID;
-const API_KEY = '$2a$10$wtqvFX6bX6lJ2EGZmY3aV.YXoR8MB7gJe...P/FQVWxutgasC35t2';//import.meta.env.VITE_API_KEY;
+const API_KEY = "$2a$10$wtqvFX6bX6lJ2EGZmY3aV.YXoR8MB7gJe...P/FQVWxutgasC35t2"; //import.meta.env.VITE_API_KEY;
 const JSONBIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
 onMounted(async () => {
   try {
     // Fetch visitor count from JSONBin
     const response = await axios.get(JSONBIN_URL, {
-      headers: { "X-Master-Key": API_KEY }
+      headers: { "X-Master-Key": API_KEY },
     });
 
     visitorCount.value = response.data.record.visitorCount + 1;
@@ -40,7 +39,6 @@ onMounted(async () => {
       { visitorCount: visitorCount.value },
       { headers: { "X-Master-Key": API_KEY } }
     );
-
   } catch (error) {
     console.error("Error updating visitor count:", error);
   }
